1192bb04ef8ed8d34b20aa2be1e55f654652ff76
oluwasanmi <sanmi_aderibigbe@outlook.com>
2017-09-24 16:54:16 +0100

    Implemented model class.

---
+++ app/src/main/java/com/sanmiaderibigbe/criminalintentreloaded/Crime.java
@@ -0,0 +1,57 @@
+package com.sanmiaderibigbe.criminalintentreloaded;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by oluwasanmi on 9/24/17.
+ */
+
+public class Crime
+{
+    private UUID mID;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime()
+    {
+        mID = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getID()
+    {
+        return mID;
+    }
+
+    public String getTitle()
+    {
+        return mTitle;
+    }
+
+    public void setTitle(String title)
+    {
+        mTitle = title;
+    }
+
+    public Date getDate()
+    {
+        return mDate;
+    }
+
+    public void setDate(Date date)
+    {
+        mDate = date;
+    }
+
+    public boolean isSolved()
+    {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved)
+    {
+        mSolved = solved;
+    }
+}
